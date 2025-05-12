254f673dc797968f51c60607cd89008526c7242a
atharva2125 <atharvaprabhu17@gmail.com>
2025-05-12 21:26:27 +0530

    made changes

--- flask/app4.py
+++ flask/app4.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/user/<name>')
-def greet_user(name):
-    return f"<h1>Hello, {name}! Welcome to our website.</h1>"
+@app.route('/')
+def greet_user():
+    name = request.args.get('name')
+    city = request.args.get('city')
+
+    if name and city:
+        return render_template('greeting.html', name=name, city=city)
+    else:
+        return '''
+            <form action="/" method="get">
+                Name: <input type="text" name="name"><br>
+                City: <input type="text" name="city"><br>
+                <input type="submit" value="Submit">
+            </form>
+        '''
 
 if __name__ == '__main__':
     app.run(debug=True)
